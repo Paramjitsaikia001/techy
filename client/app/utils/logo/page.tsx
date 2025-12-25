45b41e29f44d943697e93df20524219e2e82d3f7
Paramjit Saikia <paramjitsaikia@gmail.com>
2025-12-25 23:53:25 +0530

    creating navber

---
+++ client/app/utils/logo/page.tsx
@@ -0,0 +1,6 @@
+export default function Logo(){
+    return(
+    <h1 className="text-3xl font-sans font-bold leading-10 tracking-tight">Tech
+    <span className="text-[#0095ff] font-black">Y</span></h1>
+    )
+}
---
+++ client/app/utils/styles/page.tsx
@@ -0,0 +1,4 @@
+export const buttons={
+    primary: "px-3 py-3 flex items-center gap-2 rounded-4xl text-2xl font-medium hover:bg-[#1a1a1a] "
+
+}
